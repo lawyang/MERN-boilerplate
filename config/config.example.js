b720107b2adf78a1570e2a01fe004264d4ac978d
Law Yang <lawtouayang@gmail.com>
2019-04-09 11:00:46 -0500

    config

--- config/config.example.js
+++ config/config.example.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: 'mongodb://username:password@url:port/db',
-  db_dev: 'mongodb://url:port/db',
+  db: 'mongodb://exampleUser:12345@localhost:27017/my_db_name',
+  db_dev: 'mongodb://exampleUser:12345@localhost:27017/my_db_name',
 };
